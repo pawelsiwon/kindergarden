b0893b6b3015833c5179b8445b716b1beb841c11
Aleks4 <aleks4@outlook.com>
2019-01-10 23:14:07 +0100

    Podstawowa konfiguracja, testowy endpoint

---
+++ src/main/java/config/WebConfig.java
@@ -0,0 +1,13 @@
+package config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest();
+    }
+}
