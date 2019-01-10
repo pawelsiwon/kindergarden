50e9f2d9b340016a7d78876372eb2c062f232aaa
Aleks4 <aleks4@outlook.com>
2019-01-11 00:15:31 +0100

    Testowy endpoint i zabawy configiem

--- src/main/java/pl/kielce/tu/przedszkole/przedszkole/config/WebConfig.java
+++ src/main/java/pl/kielce/tu/przedszkole/przedszkole/config/WebConfig.java
@@ -9,9 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
+
+        http.authorizeRequests()
                 .anyRequest()
                 .permitAll();
+
     }
 }
--- src/main/java/pl/kielce/tu/przedszkole/przedszkole/controller/LoginController.java
+++ src/main/java/pl/kielce/tu/przedszkole/przedszkole/controller/LoginController.java
@@ -9,20 +9,29 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.logging.Logger;
 
 @RestController
+@RequestMapping("/api")
 public class LoginController {
 
     private static Logger logger = Logger.getLogger(LoginController.class.getName());
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
+   /* @RequestMapping(value = "/login", method = RequestMethod.POST)
     ResponseEntity<?> loginProcedure(@RequestBody LoginData login, HttpServletRequest httpServletRequest) {
         logger.info("Otrzymany login i haslo to: "+login.getLogin()+":"+login.getPassword());
         if(login.getLogin().equals("admin") && login.getPassword().equals("admin")) {
             return ResponseEntity.ok("Zalogowalo Cie... chyba.");
         }
         else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("fail.");
-    }
+    }*/
+   @RequestMapping(value="/login/{login}/{password}", method = RequestMethod.GET)
+   ResponseEntity<?> loginProcedure(@PathVariable(name = "login") String login,@PathVariable(name = "password") String password, HttpServletRequest httpServletRequest) {
 
-    @GetMapping("/test")
+       if(login.equals("admin") && password.equals("admin")) {
+           return ResponseEntity.ok("Zalogowalo Cie... chyba.");
+       }
+       else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("fail.");
+   }
+
+    @RequestMapping(value="/test", method=RequestMethod.GET)
     ResponseEntity<?> test() {
         return ResponseEntity.ok("reee");
     }
