d352170d689266aaf0fb12f863e7b8affcc9e736
Aleks4 <aleks4@outlook.com>
2019-01-10 18:01:31 +0100

    first commit

---
+++ src/main/java/pl/kielce/tu/przedszkole/przedszkole/PrzedszkoleApplication.java
@@ -0,0 +1,14 @@
+package pl.kielce.tu.przedszkole.przedszkole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrzedszkoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrzedszkoleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/pl/kielce/tu/przedszkole/przedszkole/PrzedszkoleApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.kielce.tu.przedszkole.przedszkole;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrzedszkoleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
