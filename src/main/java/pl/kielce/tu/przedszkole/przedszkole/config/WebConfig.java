4ea9da136df6c091f2fdf459d9fb75d496c4812d
Aleks4 <aleks4@outlook.com>
2019-01-11 16:38:56 +0100

    Dodane nagłówki do odpowiedzi serwera.

--- src/main/java/pl/kielce/tu/przedszkole/przedszkole/config/WebConfig.java
+++ src/main/java/pl/kielce/tu/przedszkole/przedszkole/config/WebConfig.java
@@ -4,18 +4,21 @@ package pl.kielce.tu.przedszkole.przedszkole.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+@CrossOrigin
 public class WebConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 .csrf().disable()
-                .cors().disable()
                 .authorizeRequests()
                 .anyRequest()
                 .permitAll();
 
     }
+
+
 }
--- src/main/java/pl/kielce/tu/przedszkole/przedszkole/controller/LoginController.java
+++ src/main/java/pl/kielce/tu/przedszkole/przedszkole/controller/LoginController.java
@@ -14,11 +14,14 @@ public class LoginController {
 
     private static Logger logger = Logger.getLogger(LoginController.class.getName());
 
+    @CrossOrigin
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     ResponseEntity<?> loginProcedure(@RequestBody LoginData login, HttpServletRequest httpServletRequest) {
+
         logger.info("Otrzymany login i haslo to: "+login.getLogin()+":"+login.getPassword());
         if(login.getLogin().equals("admin") && login.getPassword().equals("admin")) {
-            return ResponseEntity.ok("Zalogowalo Cie... chyba.");
+
+            return addCorsHeaders(ResponseEntity.ok("Zalogowalo Cie... chyba."));
         }
         else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("fail.");
     }
@@ -31,6 +34,17 @@ public class LoginController {
        else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("fail.");
    }
     */
+
+    ResponseEntity<?> addCorsHeaders(ResponseEntity rsp) {
+        rsp.getHeaders().add("Access-Control-Allow-Origin", "*");
+        rsp.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
+        rsp.getHeaders().add("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        rsp.getHeaders().add("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
+        rsp.getHeaders().add("Access-Control-Allow-Credentials", "true");
+
+        return rsp;
+    }
+
     @RequestMapping(value="/test", method=RequestMethod.GET)
     ResponseEntity<?> test() {
         return ResponseEntity.ok("reee");
