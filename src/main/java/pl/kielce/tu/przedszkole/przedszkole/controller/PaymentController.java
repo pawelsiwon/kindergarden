1274b8c8e51ab0b2805e0b4794a853507ae280f3
Aleks4 <aleks4@outlook.com>
2019-01-19 21:06:22 +0100

    Created dispatcher and controller for payments

---
+++ src/main/java/pl/kielce/tu/przedszkole/przedszkole/controller/PaymentController.java
@@ -0,0 +1,114 @@
+package pl.kielce.tu.przedszkole.przedszkole.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.kielce.tu.przedszkole.przedszkole.dto.Message;
+import pl.kielce.tu.przedszkole.przedszkole.dto.PaymentActionDto;
+import pl.kielce.tu.przedszkole.przedszkole.security.custom.CustomLoginUtility;
+import pl.kielce.tu.przedszkole.przedszkole.service.PaymentProxyDispatcher;
+
+import javax.naming.AuthenticationException;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/payment")
+public class PaymentController {
+
+    private final CustomLoginUtility customLoginUtility;
+    private final PaymentProxyDispatcher paymentProxyDispatcher;
+
+
+    public PaymentController(CustomLoginUtility customLoginUtility,
+                             PaymentProxyDispatcher paymentProxyDispatcher) {
+        this.customLoginUtility = customLoginUtility;
+        this.paymentProxyDispatcher = paymentProxyDispatcher;
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    ResponseEntity<?> addPayment(@RequestBody PaymentActionDto paymentActionDto) {
+        Message message;
+        try {
+            customLoginUtility.validateAuthentication(paymentActionDto.getLoginData());
+            paymentProxyDispatcher.addPayment(paymentActionDto);
+            message = new Message(200, "Added payment");
+            return ResponseEntity.ok(message);
+        }catch(AuthenticationException e) {
+            message = new Message(401, "Invalid auth data");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    ResponseEntity<?> editPayment(@RequestBody PaymentActionDto paymentActionDto) {
+        Message message;
+        try {
+            customLoginUtility.validateAuthentication(paymentActionDto.getLoginData());
+            paymentProxyDispatcher.editPayment(paymentActionDto);
+            message = new Message(200, "Edited payment");
+            return ResponseEntity.ok(message);
+        }catch(AuthenticationException e) {
+            message = new Message(401, "Invalid auth data");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    ResponseEntity<?> deletePayment(@RequestBody PaymentActionDto paymentActionDto) {
+        Message message;
+        try {
+            customLoginUtility.validateAuthentication(paymentActionDto.getLoginData());
+            paymentProxyDispatcher.deletePayment(paymentActionDto);
+            message = new Message(200, "Deleted payment");
+            return ResponseEntity.ok(message);
+        }catch(AuthenticationException e) {
+            message = new Message(401, "Invalid auth data");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @RequestMapping(value = "/list/parent_and_child", method = RequestMethod.POST)
+    ResponseEntity<?> getPaymentByParentAndChild(@RequestBody PaymentActionDto paymentActionDto) {
+        Message message;
+        try {
+            customLoginUtility.validateAuthentication(paymentActionDto.getLoginData());
+            return ResponseEntity.ok(paymentProxyDispatcher.getPaymentByParentAndChildIds(paymentActionDto));
+        }catch(AuthenticationException e) {
+            message = new Message(401, "Invalid auth data");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @RequestMapping(value = "/details", method = RequestMethod.POST)
+    ResponseEntity<?> getPaymentById(@RequestBody PaymentActionDto paymentActionDto) {
+        Message message;
+        try {
+            customLoginUtility.validateAuthentication(paymentActionDto.getLoginData());
+            return ResponseEntity.ok(paymentProxyDispatcher.getPaymentById(paymentActionDto));
+
+        }catch(AuthenticationException e) {
+            message = new Message(401, "Invalid auth data");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
--- src/main/java/pl/kielce/tu/przedszkole/przedszkole/service/PaymentProxyDispatcher.java
+++ src/main/java/pl/kielce/tu/przedszkole/przedszkole/service/PaymentProxyDispatcher.java
@@ -1,7 +1,66 @@
 package pl.kielce.tu.przedszkole.przedszkole.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.kielce.tu.przedszkole.przedszkole.dto.PaymentActionDto;
+import pl.kielce.tu.przedszkole.przedszkole.model.Payment;
+import pl.kielce.tu.przedszkole.przedszkole.model.Person;
+import pl.kielce.tu.przedszkole.przedszkole.service.PaymentService.PaymentService;
+import pl.kielce.tu.przedszkole.przedszkole.service.PaymentService.PaymentServiceImpl;
+import pl.kielce.tu.przedszkole.przedszkole.service.PaymentService.PaymentServiceParentProxy;
+import pl.kielce.tu.przedszkole.przedszkole.service.PaymentService.PaymentServiceTeacherProxy;
+import pl.kielce.tu.przedszkole.przedszkole.utils.PersonResolverUtils;
+
+import java.util.List;
 
 @Service
 public class PaymentProxyDispatcher {
+
+    private final PersonResolverUtils personResolverUtils;
+    private final PaymentServiceImpl paymentServiceImpl;
+    private final PaymentServiceTeacherProxy paymentServiceTeacherProxy;
+    private final PaymentServiceParentProxy paymentServiceParentProxy;
+
+    @Autowired
+    public PaymentProxyDispatcher(PersonResolverUtils personResolverUtils,
+                                  PaymentServiceImpl paymentServiceImpl,
+                                  PaymentServiceTeacherProxy paymentServiceTeacherProxy,
+                                  PaymentServiceParentProxy paymentServiceParentProxy) {
+        this.personResolverUtils = personResolverUtils;
+        this.paymentServiceImpl = paymentServiceImpl;
+        this.paymentServiceTeacherProxy = paymentServiceTeacherProxy;
+        this.paymentServiceParentProxy = paymentServiceParentProxy;
+    }
+
+    private PaymentService resolvePaymentInterface(Person person) {
+        if (person.getRole().equalsIgnoreCase("ADMIN")) return paymentServiceImpl;
+        else if(person.getRole().equalsIgnoreCase("TEACHER")) return paymentServiceTeacherProxy;
+        else return paymentServiceParentProxy;
+    }
+
+    @Transactional
+    public void addPayment(PaymentActionDto paymentActionDto) throws Exception {
+        resolvePaymentInterface(personResolverUtils.resolvePersonByLogin(paymentActionDto.getLoginData().getLogin()))
+                .addPayment(paymentActionDto);
+    }
+    @Transactional
+    public void editPayment(PaymentActionDto paymentActionDto) throws Exception {
+        resolvePaymentInterface(personResolverUtils.resolvePersonByLogin(paymentActionDto.getLoginData().getLogin()))
+        .editPayment(paymentActionDto);
+    }
+    @Transactional
+    public void deletePayment(PaymentActionDto paymentActionDto) throws Exception {
+        resolvePaymentInterface(personResolverUtils.resolvePersonByLogin(paymentActionDto.getLoginData().getLogin()))
+                .deletePayment(paymentActionDto);
+    }
+    public List<Payment> getPaymentByParentAndChildIds(PaymentActionDto paymentActionDto) {
+        return resolvePaymentInterface(personResolverUtils.resolvePersonByLogin(paymentActionDto.getLoginData().getLogin()))
+                .getPaymentsByParentAndChild(paymentActionDto);
+    }
+
+    public Payment getPaymentById(PaymentActionDto paymentActionDto) {
+        return resolvePaymentInterface(personResolverUtils.resolvePersonByLogin(paymentActionDto.getLoginData().getLogin()))
+                .getPaymentById(paymentActionDto);
+    }
 }
